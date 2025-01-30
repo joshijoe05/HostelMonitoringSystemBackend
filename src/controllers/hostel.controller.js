e0eb9e9cb37943c333730138fea93817925f0f33
Joshi <madugulajoshi011@gmail.com>
2025-01-30 20:07:20 +0530

    feat : get update and delete hostel routes

--- src/controllers/hostel.controller.js
+++ src/controllers/hostel.controller.js
@@ -30,7 +30,64 @@ const createHostel = asyncHandler(async (req, res) => {
     return res.status(201).json(new ApiResponse(201, "Hostel created Successfully", { hostel }));
 });
 
+const getAllHostels = asyncHandler(async (req, res) => {
+    const hostels = await Hostel.find();
+    if (!hostels) {
+        throw new ApiError(500, "Hostels could not be fetched");
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Hostels fetched Successfully", { hostels }));
+});
+
+
+const updateHostelById = asyncHandler(async (req, res) => {
+    const hostelId = req.params.id;
+    const data = req.body;
+
+    const updateHostelSchema = Joi.object({
+        name: Joi.string().trim().min(3).max(100),
+        caretakerIds: Joi.array().items(Joi.string().uuid()),
+        totalRooms: Joi.number().integer().min(1),
+        wings: Joi.array().items(Joi.string().trim()),
+        waterTimings: Joi.object({
+            start: Joi.string().regex(/^([0-9]{2}):([0-9]{2})\s?(AM|PM)$/),
+            end: Joi.string().regex(/^([0-9]{2}):([0-9]{2})\s?(AM|PM)$/)
+        })
+    }).min(1);
+
+    const { error } = updateHostelSchema.validate(data);
+    if (error) {
+        throw new ApiError(400, error.message);
+    }
+
+    const updatedHostel = await Hostel.findByIdAndUpdate(hostelId, { $set: data }, {
+        new: true, runValidators: true
+    },);
+
+    if (!updatedHostel) {
+        throw new ApiError(500, "Hostel could not be updated");
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Hostel updated Successfully", updatedHostel));
+});
+
+
+const deleteHostelById = asyncHandler(async (req, res) => {
+    const hostelId = req.params.id;
+
+    const deletedHostel = await Hostel.findByIdAndDelete(hostelId);
+
+    if (!deletedHostel) {
+        throw new ApiError(404, "Hostel not found");
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Hostel deleted Successfully", deletedHostel));
+});
+
 
 module.exports = {
-    createHostel
+    createHostel,
+    getAllHostels,
+    updateHostelById,
+    deleteHostelById,
 };
--- src/routes/hostel.route.js
+++ src/routes/hostel.route.js
@@ -5,6 +5,9 @@ const hostelController = require("../controllers/hostel.controller");
 const router = express.Router();
 
 router.route("/create").post(verifyAdmin, hostelController.createHostel);
+router.route("/").get(verifyAdmin, hostelController.getAllHostels);
+router.route("/update/:id").get(verifyAdmin, hostelController.updateHostelById);
+router.route("/delete/:id").get(verifyAdmin, hostelController.deleteHostelById);
 
 
 module.exports = router;
