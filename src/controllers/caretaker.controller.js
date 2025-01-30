641104613f8b87dcdd3f881d5f1a94e5fbf15827
Joshi <madugulajoshi011@gmail.com>
2025-01-30 20:50:23 +0530

    feat : caretaker get update delete routes

--- src/controllers/caretaker.controller.js
+++ src/controllers/caretaker.controller.js
@@ -19,14 +19,61 @@ const createCaretaker = asyncHandler(async (req, res) => {
         throw new ApiError(500, "Caretaker not created");
     }
 
-    const hostel = await Hostel.findById(hostelId);
-    hostel.caretakerIds.push(caretaker._id);
-    await hostel.save();
-
     return res.status(201).json(new ApiResponse(201, "Caretaker created Successfully", { caretaker }));
+});
+
+const getAllCaretakers = asyncHandler(async (req, res) => {
+    const caretakers = await User.find({ role: "caretaker" });
+    if (!caretakers) {
+        throw new ApiError(404, "Caretakers not found");
+    }
+    return res.status(200).json(new ApiResponse(200, "Caretakers fetched Successfully", { caretakers }));
+});
+
+
+const updateCaretakerById = asyncHandler(async (req, res) => {
+    const caretakerId = req.params.id;
+    const { fullName, hostelId, contactNumber } = req.body;
+
+    const updateCaretakerSchema = Joi.object({
+        fullName: Joi.string().trim().min(3).max(100),
+        hostelId: Joi.string().uuid(),
+        contactNumber: Joi.string().trim().min(10).max(10)
+    }).min(1);
+
+    const { error } = updateCaretakerSchema.validate({ fullName, hostelId, contactNumber });
+    if (error) {
+        throw new ApiError(400, error.message);
+    }
+
+    const updatedCaretaker = await User.findByIdAndUpdate(caretakerId, { $set: data }, {
+        new: true, runValidators: true
+    },);
+
+    if (!updatedCaretaker) {
+        throw new ApiError(500, "Caretaker could not be updated");
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Caretaker updated Successfully", updatedCaretaker));
+});
+
+
+const deleteCaretakerById = asyncHandler(async (req, res) => {
+    const caretakerId = req.params.id;
+
+    const deletedCaretaker = await Hostel.findByIdAndDelete(hostelId);
+
+    if (!deletedCaretaker) {
+        throw new ApiError(404, "Caretaker not found");
+    }
+
+    return res.status(200).json(new ApiResponse(200, "Hostel deleted Successfully", deletedCaretaker));
 });
 
 
 module.exports = {
     createCaretaker,
+    getAllCaretakers,
+    updateCaretakerById,
+    deleteCaretakerById,
 }
--- src/routes/caretaker.routes.js
+++ src/routes/caretaker.routes.js
@@ -5,5 +5,8 @@ const verifyAdmin = require("../middlewares/admin.middleware");
 const router = express.Router();
 
 router.route("/create").post(verifyAdmin, careTakerController.createCaretaker);
+router.route("/").get(verifyAdmin, careTakerController.getAllCaretakers);
+router.route("/update/:id").put(verifyAdmin, careTakerController.updaateCaretakerById);
+router.route("/delete/:id").delete(verifyAdmin, careTakerController.deleteCaretakerById);
 
 module.exports = router;
