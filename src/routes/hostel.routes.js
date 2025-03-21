c78528597d41ca495dc5c5527b1e4bd814a5a0ef
Joshi <madugulajoshi011@gmail.com>
2025-03-21 22:21:57 +0530

    fix : get all hostels authorization changed

--- src/routes/hostel.routes.js
+++ src/routes/hostel.routes.js
@@ -5,7 +5,7 @@ const hostelController = require("../controllers/hostel.controller");
 const router = express.Router();
 
 router.route("/create").post(authorizeUserRoles("admin"), hostelController.createHostel);
-router.route("/").get(authorizeUserRoles("admin"), hostelController.getAllHostels);
+router.route("/").get(hostelController.getAllHostels);
 router.route("/update/:id").put(authorizeUserRoles("admin"), hostelController.updateHostelById);
 router.route("/delete/:id").delete(authorizeUserRoles("admin"), hostelController.deleteHostelById);
 
