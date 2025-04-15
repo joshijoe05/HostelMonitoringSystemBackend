9cab2da6de5612fcb8e928c5e57e920fe73172dc
Joshi <madugulajoshi011@gmail.com>
2025-04-15 23:52:01 +0530

    feat : added role in access token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -90,7 +90,8 @@ userSchema.methods.generateAccessToken = function () {
         {
             _id: this._id,
             email: this.email,
-            fullName: this.fullName
+            fullName: this.fullName,
+            role: this.role
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
